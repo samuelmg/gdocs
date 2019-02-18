fe878176bd6550ecae61e2bc5b32037a8675677b
Samuel Mercado <samuel.mg@gmx.com>
2019-02-18 16:49:26 -0600

    Crea vistas contacto e info

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/info', function() {
+    //return "Información del Sistema";
+    return view('paginas.informacion');
+});
+
+Route::get('/contacto', function() {
+    return view('paginas.contacto');
+});
