22cba85a7238599364b16ab0790d1c1a5c4528dc
Samuel Mercado <samuel.mg@protonmail.com>
2019-03-13 15:09:51 -0600

    Crea layout tabler y vistas parciales

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,11 @@
+<ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+  <li class="nav-item">
+    <a href="/" class="nav-link"><i class="fe fe-home"></i> Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown"><i class="fe fe-box"></i> Documentos</a>
+    <div class="dropdown-menu dropdown-menu-arrow">
+      <a href="#" class="dropdown-item ">Listado</a>
+    </div>
+  </li>
+</ul>
