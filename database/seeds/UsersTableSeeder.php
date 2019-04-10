6f77690371fdef497be223770120570630496a70
Samuel Mercado <samuel.mg@protonmail.com>
2019-04-10 13:45:34 -0500

    Agrega rol a tabla users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +12,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $admin = new App\User();
+        $admin->name = 'Administrador';
+        $admin->email = 'admin@prueba.com';
+        $admin->rol = 'admin';
+        $admin->email_verified_at = now();
+        $admin->password = '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm';
+        $admin->remember_token = Str::random(10);
+        $admin->save();
+
         factory(App\User::class, 15)->create();
     }
 }
