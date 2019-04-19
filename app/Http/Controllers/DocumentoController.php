128c9c9fc24b69dc2021d406782d843cc62dcc36
Samuel Mercado <samuel.mg@protonmail.com>
2019-04-19 13:36:44 -0500

    Implementa middleware auth y admin en DocumentoController
    Implementa middleware auth y admin desde constructor de DocumentoController
    Se especifican los métodos a los cuales se aplican cada uno

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -10,6 +10,12 @@ use Illuminate\Http\Request;
 
 class DocumentoController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index', 'show');
+        $this->middleware('admin')->only('create', 'store', 'edit', 'update', 'destroy');
+    }
+
     /**
      * Display a listing of the resource.
      *
